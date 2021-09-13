19c9ad8b39b927971ea047e9c1b0a72de9f1ec55
SudhanshuVijay0 <78337703+SudhanshuVijay0@users.noreply.github.com>
2021-09-13 12:07:44 +0530

    Second Push

--- imagebazar/myapp/forms.py
+++ imagebazar/myapp/forms.py
@@ -1,8 +1,20 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.forms.widgets import PasswordInput
+
 
 class CreateUserForm(UserCreationForm):
+	password1 = forms.CharField(max_length=16, widget=forms.PasswordInput(
+	    attrs={'class': 'form-control', 'placeholder': 'New Password'}))
+    
+	password2 = forms.CharField(max_length=16, widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
+	
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
+		widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Username'}),
+			'email': forms.EmailInput(attrs={'placeholder':'Email'}),
+			
+        }
